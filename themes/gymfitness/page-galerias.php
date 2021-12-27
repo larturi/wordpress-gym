d2fda515c666b9743c4e8651ce08080c3cde3ab2
lea.arturi <lea.arturi@gmail.com>
2021-12-27 18:39:14 -0300

    Galeria lista

--- themes/gymfitness/page-galerias.php
+++ themes/gymfitness/page-galerias.php
@@ -1,7 +1,4 @@
-<?php get_header(); ?>
-    /*
-        Template Name: Template Galerias
-    */ 
+<?php get_header(); ?> 
     <main class="contenedor pagina seccion">
         <div class="contenido-principal">
             <?php while(have_posts()): the_post(); ?>
@@ -12,7 +9,21 @@
                 ?>
 
                 <ul class="galeria-imagenes">
-                    
+                    <?php 
+                        $i = 1;
+                        foreach ($galeria_imagenes_ids as $id):
+                            $size = ($i == 4 || $i == 6) ? 'portrait' : 'square';
+                            $imagenThumb = wp_get_attachment_image_src($id, $size)[0];
+                            $imagen = wp_get_attachment_image_src($id, 'full')[0];
+                    ?>    
+
+                        <li>
+                            <a href="<?php echo $imagen; ?>" data-lightbox="galeria">
+                                <img src="<?php echo $imagenThumb; ?>" alt="Imagen">
+                            </a>
+                        </li>
+                        
+                    <?php $i++; endforeach; ?>
                 </ul>
             <?php endwhile; ?>
         </div>
