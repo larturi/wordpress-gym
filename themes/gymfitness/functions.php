e7d95ce643fc2142760986949563c11a3dc0a6ae
lea.arturi <lea.arturi@gmail.com>
2021-12-29 09:21:31 -0300

    Header principal

--- themes/gymfitness/functions.php
+++ themes/gymfitness/functions.php
@@ -81,3 +81,22 @@ function gymfitness_widgets() {
     ));
 }
 add_action('widgets_init', 'gymfitness_widgets');
+
+// Imagen Hero
+function gymfitness_imagen_hero() {
+    $front_page_id = get_option('page_on_front');
+    $id_imagen = get_field('imagen_hero', $front_page_id);
+    $imagen = wp_get_attachment_image_src($id_imagen, 'full')[0];
+    
+    wp_register_style('custom', false);
+    wp_enqueue_style('custom');
+
+    $imagen_destacada_css = "
+        body.home .site-header {
+            background-image: linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url('{$imagen}');
+        }
+    ";
+
+    wp_add_inline_style('custom', $imagen_destacada_css);
+}
+add_action('init', 'gymfitness_imagen_hero');
---
+++ themes/gymfitness/header-front.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    
+</body>
+
+<header class="site-header">
+    <div class="contenedor header-grid">
+        <div class="barra-navegacion">
+
+            <div class="logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo Sitio">
+            </div>
+
+            <?php
+                $args = array(
+                    'theme_location' => 'menu-principal',
+                    'container' => 'nav',
+                    'container_class' => 'menu-principal'
+                );
+                wp_nav_menu($args);
+            ?>
+
+        </div>
+
+        <div class="tagline text-center">
+            <h1>
+                <?php the_field('encabezado_hero'); ?>
+                <p>
+                    <?php the_field('contenido_hero'); ?>
+                </p>
+            </h1>
+        </div>
+    </div>
+</header>
