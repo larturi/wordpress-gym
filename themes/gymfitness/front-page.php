0ed86cb0c9d37e5522e381936ca4de09947f5a83
lea.arturi <lea.arturi@gmail.com>
2021-12-30 11:38:47 -0300

    Profesores

--- themes/gymfitness/front-page.php
+++ themes/gymfitness/front-page.php
@@ -43,4 +43,45 @@
     </div>
 </section>
 
+<section class="instructores">
+    <div class="contenedor seccion">
+        <h2 class="text-center texto-primario">
+            Nuestros Instructores
+        </h2>
+        
+        <p class="text-center">
+            <?php the_field('instructores_parrafo'); ?>
+        </p>
+
+        <ul class="listado-instructores">
+            <?php 
+                $args = array(
+                    'post_type' => 'instructores',
+                    'posts_per_page' => -1,
+                    'order' => 'ASC'
+                );
+                $instructores = new WP_Query($args);
+                while($instructores->have_posts()):
+                    $instructores->the_post();   
+            ?>
+            <li class="instructor">
+                <?php the_post_thumbnail('mediano'); ?>
+                <div class="contenido text-center">
+                    <h3><?php the_title(); ?></h3>
+                    <?php the_content(); ?>
+                    <div class="especialidad">
+                        <?php 
+                            $esp = get_field('especialidad'); 
+                            foreach($esp as $e):
+                        ?>
+                            <span class="etiqueta"><?php echo esc_html($e); ?></span>
+                        <?php endforeach; ?>
+                    </div>
+                </div>
+            </li>
+            <?php endwhile; wp_reset_postdata(); ?>
+        </ul>
+    </div>
+</section>
+
 <?php get_footer() ?>
