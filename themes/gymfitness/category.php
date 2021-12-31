8ba558eb56ac2c34daaad8092c7cd9a0aacb5e77
lea.arturi <lea.arturi@gmail.com>
2021-12-31 10:49:24 -0300

    Finalizado

--- themes/gymfitness/category.php
+++ themes/gymfitness/category.php
@@ -7,7 +7,9 @@
             ?>
         </h2>
         <ul class="listado-blog">
-            <?php get_template_part('template-parts/loop', 'blog') ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <?php get_template_part('template-parts/loop', 'blog') ?>
+            <?php endwhile; ?>
         </ul>
     </main>
 <?php get_footer(); ?>
